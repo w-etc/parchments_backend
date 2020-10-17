beecf3df376602f181277d35597c055282b7e0d9
w-etc <luwolf7@gmail.com>
2020-10-17 19:38:12 -0300

    saving synopsis in parchment creation endpoints

--- src/main/java/parchments_backend/repositories/ParchmentRepository.java
+++ src/main/java/parchments_backend/repositories/ParchmentRepository.java
@@ -9,10 +9,10 @@ import java.util.Optional;
 
 public interface ParchmentRepository extends Neo4jRepository<Parchment, Long> {
 
-    @Query("MATCH (w:Writer) WHERE id(w) = $writerId CREATE (w)-[:WROTE]->(p:Parchment{title:$s.title, contents:$s.contents}) RETURN p")
+    @Query("MATCH (w:Writer) WHERE id(w) = $writerId CREATE (w)-[:WROTE]->(p:Parchment{title:$s.title, synopsis:$s.synopsis, contents:$s.contents}) RETURN p")
     Parchment save(Parchment s, Long writerId);
 
-    @Query("MATCH (w:Writer) WHERE id(w) = $writerId MATCH (pre:Parchment) WHERE id(pre) = $previousParchmentId CREATE (w)-[:WROTE]->(p:Parchment{title:$s.title, contents:$s.contents})<-[:CONTINUATION]-(pre) RETURN p")
+    @Query("MATCH (w:Writer) WHERE id(w) = $writerId MATCH (pre:Parchment) WHERE id(pre) = $previousParchmentId CREATE (w)-[:WROTE]->(p:Parchment{title:$s.title, synopsis:$s.synopsis, contents:$s.contents})<-[:CONTINUATION]-(pre) RETURN p")
     Parchment save(Parchment s, Long writerId, Long previousParchmentId);
 
     @Query("MATCH (w:Writer)-[wr:WROTE]->(p:Parchment) WHERE id(w) = $writerId return p")
