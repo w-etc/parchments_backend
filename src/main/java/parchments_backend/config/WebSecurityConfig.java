0753967839fb2006efbd8b61c52a86cef4afc547
w-etc <luwolf7@gmail.com>
2020-11-07 20:07:22 -0300

    fixing issue in which logged in users would not be able to create parchments

--- src/main/java/parchments_backend/config/WebSecurityConfig.java
+++ src/main/java/parchments_backend/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable()
-                .authorizeRequests().antMatchers(HttpMethod.POST, "/parchment/*").authenticated()
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/parchment").authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
